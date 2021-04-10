e61904f65947ce4a3b2712ec6e0a8cfb11e6f537
Lucio Palmieri <lpalmieri@promoqui.it>
2021-04-10 21:13:44 +0200

    Add docstring to DMediaEngine.add_video_processor

--- dddpy/dddpy-example.py
+++ dddpy/dddpy-example.py
@@ -1,15 +1,13 @@
 from dddpy.media.engine import DMediaEngine
 
-from dddpy.media.video_processors import DVideoProcGrayscale
 from dddpy.media.video_processors import DVideoProcFreeze
+from dddpy.media.video_processors import DVideoProcGrayscale
 
 
 def main():
 
     media_engine = DMediaEngine(close_key='q')
 
-    # add_video_processor return the id(video_proc_dict) that can be used to programmatically toggle the processor
-    #   activation without using the toggle_key
     media_engine.add_video_processor(DVideoProcFreeze(), toggle_key='f')
     media_engine.add_video_processor(DVideoProcGrayscale(), toggle_key='b')
 
